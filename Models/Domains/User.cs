b3e90bbadece36f3608fe95f138650ce43eb3571
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-06-01 13:17:10 +0530

    started project

---
+++ Models/Domains/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fleet_management_backend.Models.Domains
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string? Email { get; set; }
+
+        [Required]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [ForeignKey("UserType")]
+        public Guid UserTypeId { get; set; }
+
+        public UserType UserType { get; set; }
+    }
+}
